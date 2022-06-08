f23cf22bc694aa406c98f3828c72f5c89fe5b432
Cix8 <francesco.natal_fn@libero.it>
2022-06-08 15:24:20 +0200

    details modified

--- snack4/index.php
+++ snack4/index.php
@@ -4,7 +4,7 @@ $result_array = [];
 $max_len_array = 15;
 
 while (count($result_array) < $max_len_array) {
-    $number = rand(1,15);
+    $number = rand(1,50);
     if (!in_array($number, $result_array)) {
         $result_array[] = $number;
     }
